edc18eef1c96c7f0cffa1a32f3a0bb55f11cde5f
pndungutse <pndungutse1@gmail.com>
2021-09-22 18:05:57 +0200

    about us and blog first commit

--- core/views.py
+++ core/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def home(request):
     template_name = 'pages/home.html'
     return render(request, template_name)
+
+def aboutUs(request):
+    template_name = 'pages/about-us.html'
+    return render(request, template_name)
+
+def blog(request):
+    template_name = 'pages/blog.html'
+    return render(request, template_name)
+
+def blogDetail(request):
+    template_name = 'pages/blog-detail.html'
+    return render(request, template_name)
