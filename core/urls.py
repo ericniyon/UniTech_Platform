3b053220bd666e9898ca4c7cc6fc50ef85563629
DESKTOP-UV1HOGK\Celestine <cyubahironoel@gmail.com>
2021-10-17 02:18:54 +0200

    add new changes on blog

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
-
+from . import views
 urlpatterns = [
     path('', home, name='home'),
     path('login', loginOrg, name='login'),
     path('logout', logout_view, name='logout'),
     path('dashboard', dashboard, name='dashboard'),
     path('about-us', aboutUs, name='about-us'),
-    path('blog-grid', blog, name='blog-grid'),
-    path('blog-detail', blogDetail, name='blog-detail'),
+    path('blog-grid', views.PostList.as_view(), name='blog-grid'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='blog-detail'),
     path('services', services, name='services'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
+from django.views import generic
+from .models import Post
 
 
 def loginOrg(request):
@@ -13,34 +15,51 @@ def loginOrg(request):
             return redirect('dashboard')
     else:
         form = AuthenticationForm()
-    return render(request, 'login.html', {'form':form})
+    return render(request, 'login.html', {'form': form})
+
 
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('login')
 
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'pages/blog.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'pages/blog-detail.html'
+
+
 def home(request):
     template_name = 'pages/home.html'
     return render(request, template_name)
+
 
 def aboutUs(request):
     template_name = 'pages/about-us.html'
     return render(request, template_name)
 
+
 def blog(request):
     template_name = 'pages/blog.html'
     return render(request, template_name)
+
 
 def blogDetail(request):
     template_name = 'pages/blog-detail.html'
     return render(request, template_name)
 
+
 def services(request):
     template_name = 'pages/services.html'
-    return render(request, template_name) 
+    return render(request, template_name)
+
 
 def dashboard(request):
-    
+
     context = {}
-    return render(request, 'dashboard.html',context)
+    return render(request, 'dashboard.html', context)
